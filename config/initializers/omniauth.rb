7fffd2d6754f2fa96d8cd92e5ffb85395379cb4f
keerthik <keerthik@bitgym.com>
2013-03-23 13:24:12 -0700

    More app secret/id stuff

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  require 'openid/store/filesystem' 
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :openid, :store => OpenID::Store::Filesystem.new('/tmp')
+end
